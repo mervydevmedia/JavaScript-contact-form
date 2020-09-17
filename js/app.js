dac4586e8fdc4e1e40d6eb80da8d6e7554879e6a
mervydevmedia <mervydevmedia@gmail.com>
2020-09-17 16:36:37 +0000

    Awesome contact form with javascript

---
+++ js/app.js
@@ -0,0 +1,64 @@
+function animatedForm() {
+   const arrows = document.querySelectorAll('.fa-arrow-circle-down');
+   arrows.forEach(arrow => {
+      arrow.addEventListener('click', () => {
+         const input = arrow.previousElementSibling;
+         const parent = arrow.parentElement;
+         const nextForm = parent.nextElementSibling;
+
+         // Check validation
+         if (input.type === 'text' && validateUser(input)) {
+            nextSlide(parent, nextForm);
+         } else if (input.type === 'email' && validateEmail(input)) {
+            nextSlide(parent, nextForm);
+         } else if (input.type === 'password' && validateUser(input)) {
+            nextSlide(parent, nextForm);
+         } else {
+            parent.style.animation = "shake 0.5s ease";
+         }
+         // Animation
+         parent.addEventListener('animationend', () => {
+            parent.style.animation = '';
+         });
+      });
+   });
+}
+
+// Validation User
+function validateUser(user) {
+   if (user.value.length < 5) {
+      // Bad
+      error('#ff6b81');
+   } else {
+      // OK
+      error('#2ed573');
+      return true;
+   }
+}
+
+// Validate Email
+function validateEmail(email) {
+   const validationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if (validationEmail.test(email.value)) {
+      // OK Color
+      error('#2ed573');
+      return true;
+   } else {
+      // Bad color
+      error('#ff6b81');
+   }
+}
+
+// nextSlide
+function nextSlide(parent, nextForm) {
+   parent.classList.add("innactive");
+   parent.classList.remove("active");
+   nextForm.classList.add("active");
+}
+
+// Error Message
+function error(color) {
+   document.body.style.backgroundColor = color;
+}
+
+animatedForm();
